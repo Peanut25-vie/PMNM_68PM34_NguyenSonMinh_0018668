32ca5bcae7c547a47061d726e3d9063a6156ef51
Nguyen Son Minh-68PM4-0018668 <minhthpthiephoa1@gmail.com>
2026-06-04 09:24:32 +0700

    Update sinhvien.php

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,16 +1,38 @@
 <?php
-class sinhvien
+require_once '../app/core/Controller.php';
+class sinhvien extends Controller
 {
   public function index()
   {
-    // trả về view
-    require_once '../app/views/sinhvien/index.php';
+    $sinhvienModel = $this->model('sinhvienModel');
+    $sinhviens = $sinhvienModel->getAllSinhVien();
+    //trả về view
+
+    //require_once '../app/views/sinhvien/index.php';
+    $this->view("layout/masterlayout", ['viewname' => 'sinhvien/index', 'sinhviens' => $sinhviens, 'title' => 'Danh sách sinh viên']);
   }
+
   public function create()
   {
-    // trả về view
     require_once '../app/views/sinhvien/create.php';
   }
+  public function store()
+  {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $data = [
+        'hoten' => $_POST['hoten'],
+        'gioitinh' => $_POST['gioitinh'],
+        'mssv' => $_POST['mssv']
+      ];
+      $sinhvienModel = $this->model('sinhvienModel');
+      $result = $sinhvienModel->create($data);
+      if ($result) {
+        $_SESSION['success'] = "Thêm sinh viên thành công!";
+        header("Location: /sinhvien/index");
+        exit();
+      } else {
+        $_SESSION['error'] = "Thêm sinh viên thất bại!";
+      }
+    }
+  }
 }
-$sv = new sinhvien();
-$sv->index();
