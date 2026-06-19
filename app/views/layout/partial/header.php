b03dfc2ff4fcb76a7999e6cfd6bf07e47544c228
Nguyen Son Minh-68PM4-0018668 <minhthpthiephoa1@gmail.com>
2026-06-19 21:20:17 +0700

    Update header.php

--- app/views/layout/partial/header.php
+++ app/views/layout/partial/header.php
@@ -1,21 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand fw-bold" href="/baitap/public/sinhvien/index">QLSV</a>
+        <a class="navbar-brand fw-bold" href="/PMNM_68PM34_NguyenSonMinh_0018668/public/sinhvien/index">QLSV</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="/baitap/public/sinhvien/index">Sinh viên</a>
+                    <a class="nav-link active" href="/PMNM_68PM34_NguyenSonMinh_0018668/public/sinhvien/index">Sinh viên</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/baitap/public/lophoc">Lớp học</a>
+                    <a class="nav-link" href="/PMNM_68PM34_NguyenSonMinh_0018668/public/lophoc">Lớp học</a>
                 </li>
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link text-danger fw-bold" href="/baitap/public/auth/logout">Đăng xuất</a>
+                    <a class="nav-link text-danger fw-bold" href="/PMNM_68PM34_NguyenSonMinh_0018668/public/auth/logout">Đăng xuất</a>
                 </li>
             </ul>
         </div>
