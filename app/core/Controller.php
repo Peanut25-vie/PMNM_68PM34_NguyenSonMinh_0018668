75d95aeb52c153d197acbe27b0dffbafb24bfe13
Nguyen Son Minh-68PM4-0018668 <minhthpthiephoa1@gmail.com>
2026-06-19 20:35:49 +0700

    Update Controller.php

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,14 +1,20 @@
 <?php
-class Controller
-{
-    public function model($model)
-    {
-        require_once '../app/models/' . $model . '.php';
-        return new $model();
+class Controller {
+    public function model($model) {
+        if (file_exists('../app/models/' . $model . '.php')) {
+            require_once '../app/models/' . $model . '.php';
+            return new $model();
+        }
+        return null;
     }
-    public function view($viewName, $data = [] )
-    {
+
+    public function view($viewName, $data = []) {
         extract($data);
-        require_once '../app/views/' . $viewName . '.php';
+        if (file_exists('../app/views/' . $viewName . '.php')) {
+            require_once '../app/views/' . $viewName . '.php';
+        } else {
+            echo "View: $viewName không tồn tại.";
+        }
     }
 }
+?>
