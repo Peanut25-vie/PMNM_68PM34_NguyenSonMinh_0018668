de5e86f1afc967f454c6d5e950590d80452c6d63
Nguyen Son Minh-68PM4-0018668 <minhthpthiephoa1@gmail.com>
2026-06-19 20:36:27 +0700

    Update sinhvienModel.php

--- app/models/sinhvienModel.php
+++ app/models/sinhvienModel.php
@@ -1,43 +1,103 @@
 <?php
 require_once '../app/core/DB.php';
+
 class sinhvienModel
 {
     private $conn;
+
     public function __construct()
     {
         $this->conn = connectDB::Connect();
     }
-    public function getAllSinhVien()
-    {
-        $query = "SELECT * FROM tbl_sinhviens";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+
     public function create($data)
     {
-        $query = "INSERT INTO tbl_sinhviens (hoten, gioitinh, mssv) VALUES (:hoten, :gioitinh, :mssv)";
+        $query = "INSERT INTO tbl_sinhviens (hoten, gioitinh, mssv, lophoc) VALUES (:hoten, :gioitinh, :mssv, :lophoc)";
         $stmt = $this->conn->prepare($query);
-        if ($stmt->execute($data)) {
-            return true;
-        } else {
-            return false;
-        }
+        return $stmt->execute([
+            ':hoten' => $data['hoten'],
+            ':gioitinh' => $data['gioitinh'],
+            ':mssv' => $data['mssv'],
+            ':lophoc' => isset($data['lophoc']) ? $data['lophoc'] : null
+        ]);
     }
-    public function paging($limit = 5, $offset = 0, $search = '')
+
+    // Hàm lấy dữ liệu có lọc tìm kiếm và phân trang
+    public function paging($limit = 10, $offset = 0, $search = '', $lophoc = '')
     {
-        $query = "SELECT * FROM tbl_sinhviens LIMIT :limit OFFSET :offset";
+        $query = "SELECT * FROM tbl_sinhviens WHERE 1=1";
+        $params = [];
+
+        if (!empty($search)) {
+            $query .= " AND (hoten LIKE :search OR mssv LIKE :search)";
+            $params[':search'] = '%' . $search . '%';
+        }
+
+        if (!empty($lophoc)) {
+            $query .= " AND lophoc = :lophoc";
+            $params[':lophoc'] = $lophoc;
+        }
+
+        // Tính tổng số bản ghi
+        $countQuery = str_replace("SELECT *", "SELECT COUNT(*)", $query);
+        $stmtCount = $this->conn->prepare($countQuery);
+        $stmtCount->execute($params);
+        $totalRecords = $stmtCount->fetchColumn();
+
+        // Truy vấn lấy dữ liệu
+        $query .= " LIMIT :limit OFFSET :offset";
         $stmt = $this->conn->prepare($query);
-        $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
-        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+
+        foreach ($params as $key => $val) {
+            $stmt->bindValue($key, $val);
+        }
+        $stmt->bindValue(':limit', (int)$limit, PDO::PARAM_INT);
+        $stmt->bindValue(':offset', (int)$offset, PDO::PARAM_INT);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        //tính tổng số bản ghi
-        $selectAllQuery = $this->conn->query("SELECT COUNT(*) FROM tbl_sinhviens");
-        //$selectAllQuery->execute();
-        $totalRecords   = $selectAllQuery->fetchColumn();
-        $totalPages     = ceil($totalRecords / $limit);
-        return ['sinhviens' => $result, 'totalpage' => $totalPages];
+        $totalPages = ceil($totalRecords / $limit);
+
+        return [
+            'sinhviens' => $result, 
+            'totalpage' => $totalPages,
+            'totalrecords' => $totalRecords
+        ];
+    }
+
+    public function getUniqueClasses()
+    {
+        $query = "SELECT DISTINCT lophoc FROM tbl_sinhviens WHERE lophoc IS NOT NULL AND lophoc != ''";
+        $stmt = $this->conn->query($query);
+        return $stmt->fetchAll(PDO::FETCH_COLUMN);
+    }
+
+    public function getSinhVienById($id)
+    {
+        $query = "SELECT * FROM tbl_sinhviens WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([':id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function update($data)
+    {
+        $query = "UPDATE tbl_sinhviens SET hoten = :hoten, gioitinh = :gioitinh, mssv = :mssv, lophoc = :lophoc WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute([
+            ':hoten' => $data['hoten'],
+            ':gioitinh' => $data['gioitinh'],
+            ':mssv' => $data['mssv'],
+            ':lophoc' => isset($data['lophoc']) ? $data['lophoc'] : null,
+            ':id' => $data['id']
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $query = "DELETE FROM tbl_sinhviens WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute([':id' => $id]);
     }
 }
+?>
