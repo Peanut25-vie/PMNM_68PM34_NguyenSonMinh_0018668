267ec16288e4a50b348eb7a140af8a620fcc9309
Nguyen Son Minh-68PM4-0018668 <minhthpthiephoa1@gmail.com>
2026-06-19 20:42:55 +0700

    Refactor middleware methods to static and update URIs

--- app/views/middleware.php
+++ app/views/middleware.php
@@ -1,22 +1,33 @@
 <?php
-require_once '../app/core/App.php';
 class middleware
 {
-  function checklogin()
+    public static function checklogin()
     {
-        $publicPages = ['/home/login', '/auth/login'];
+      
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
 
-        if (!isset($_SESSION['username']) && !in_array($_SERVER['REQUEST_URI'], $publicPages)) {
-            header('Location: /home/login');
+       
+        $publicPages = ['/baitap/public/home/login', '/baitap/public/auth/login'];
+        $currentUri = explode('?', $_SERVER['REQUEST_URI'])[0];
+
+        
+        if (!isset($_SESSION['username']) && !in_array($currentUri, $publicPages)) {
+            header('Location: /baitap/public/home/login');
             exit();
         }
     }
 
-    public function checklogout()
+    public static function checklogout()
     {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        
+       
         if (isset($_SESSION['username'])) {
-
-            header('Location: /home/index');
+            header('Location: /baitap/public/sinhvien/index');
             exit();
         }
     }
