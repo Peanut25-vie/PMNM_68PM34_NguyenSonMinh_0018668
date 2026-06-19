695a829536bc9f2a69aabed95c37573e1dcfa686
Nguyen Son Minh-68PM4-0018668 <minhthpthiephoa1@gmail.com>
2026-06-19 20:39:40 +0700

    Update header.php

--- app/views/layout/partial/header.php
+++ app/views/layout/partial/header.php
@@ -1,29 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
-    <style>
-        .header{
-            width: 100%;
-            height: 80px;
-            background-color: red;
-        }
-    </style>
-</head>
-<body>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand fw-bold" href="/baitap/public/sinhvien/index">QLSV</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/baitap/public/sinhvien/index">Sinh viên</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/baitap/public/lophoc">Lớp học</a>
+                </li>
+            </ul>
             <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="/sinhvien">Quản lý sinh viên</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/lophoc">Quản lý lớp học</a>
-            </li>
+                <li class="nav-item">
+                    <a class="nav-link text-danger fw-bold" href="/baitap/public/auth/logout">Đăng xuất</a>
+                </li>
             </ul>
         </div>
-    </nav>
-</body>
-</html>
+    </div>
+</nav>
